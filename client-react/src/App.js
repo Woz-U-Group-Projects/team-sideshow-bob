f6d3d036738cac93e0ec05bbb26aad822cac411e
Brian Lewis <briandeanlewis@gmail.com>
2020-10-18 19:46:31 -0400

    Full Functionality - Brian

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,11 +1,16 @@
+
 import React from "react";
-import  Ticket from "./components/Ticket";
+import  Ticket from "./components/Home";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Ticket />
+
     </div>
   );
 }
