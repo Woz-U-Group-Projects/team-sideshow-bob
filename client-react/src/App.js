d5a803e4fd258ca927ea9d1e37f9c8f1a3a817e5
Scott Thompson <thompsonscott1180@gmail.com>
2020-10-19 20:09:22 -0500

    Routing app.js
    added routing to app.js

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
 import Home from "./components/home";
 // import Task from "./components/Task";
+import Navbar from "./components/Navbar/Navbar";
 import ViewTicket from "./components/ViewTicket";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div>
-      <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/view">view</Link>
-          </li>
-        </ul>
-        <h1> this is where our navbar will go that will host our links to the modules</h1>
-
-        <hr />
+        <Navbar />
 
         <Route exact path="/" component={Home} />
         <Route path="/view" component={ViewTicket} />
