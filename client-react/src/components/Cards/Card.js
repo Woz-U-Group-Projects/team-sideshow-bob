4fa6c19719a529d9514070a981c4d0af241c3fcd
Gio <ngiovannie@outlook.com>
2020-10-29 20:38:49 -0400

    Added Photos to Cards

--- client-react/src/components/Cards/Card.js
+++ client-react/src/components/Cards/Card.js
@@ -2,9 +2,8 @@ import React from 'react'
 import Card from './CardUi'
 
 // import IMG_1 from 'https://media-exp1.licdn.com/dms/image/C5103AQEFNvlsjPks7w/profile-displayphoto-shrink_200_200/0?e=1609372800&v=beta&t=9O_lO6MZ_rLRwc5Ilmy10WWlaMQ_CXI5n2ZLHPVJO1Q';
-// import IMG_2 from '../assets/IMG_2.jpg';
+import Chris_Whiting from '../assets/Chris-Whiting.jpg'
 import Beckie_Rich from '../assets/Beckie_Rich.jpg'
-
 import Thompson_Scott from '../assets/Thompson_Scott.jpg'
 
 
@@ -30,7 +29,7 @@ class Cards extends React.Component {
                         <Card imgsrc= {{}} title= "Brian Lewis" text="Junior Developer"/>
                     </div>
                     <div className="col-lx">
-                        <Card imgsrc={{}} title= "Chris Whiting" text="Junior Developer"/>
+                        <Card imgsrc={Chris_Whiting} title= "Chris Whiting" text="Junior Developer"/>
                     </div>
                     <div className="col-lx">
                         <Card imgsrc={Beckie_Rich} title= "Beckie Rich" text="Junior Developer"/>
