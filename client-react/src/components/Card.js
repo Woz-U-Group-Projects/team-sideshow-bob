505b9cd7ad1fc6fe79bb0d85222ca6b3508b1613
Brian Lewis <briandeanlewis@gmail.com>
2020-11-03 19:19:59 -0500

    Cards

--- client-react/src/components/Card.js
+++ client-react/src/components/Card.js
@@ -1,38 +1,43 @@
 import React from 'react'
 import Card from './CardUi'
+
+// import IMG_1 from 'https://media-exp1.licdn.com/dms/image/C5103AQEFNvlsjPks7w/profile-displayphoto-shrink_200_200/0?e=1609372800&v=beta&t=9O_lO6MZ_rLRwc5Ilmy10WWlaMQ_CXI5n2ZLHPVJO1Q';
 import Beckie_Rich from './Assets/Beckie_Rich.jpg'
-import Thompson_Scott from './Assets/Thompson_Scott.jpg'
+import Brian_Lewis from './Assets/Brian_Lewis.jpg'
+import Scott_Thompson from './Assets/Thompson_Scott.jpg'
+import Chris_Whiting from './Assets/Chris_Whiting.jpg'
+import Gio_Nava from './Assets/Gio_Nava.jpg'
 
 
 
 class Cards extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state={};
 
     }
 
-
-
+  
+      
 
     render() {
-        return (
-            <div className="container-fluid1 d-flex justify-content-center">
+        return(
+            <div className= "container-fluid1 d-flex justify-content-center">
                 <div className="row">
                     <div className="col-lx">
-                        <Card imgsrc={{}} title="Giovanni Nava" text="Junior Developer"></Card>
+                        <Card imgsrc= {Brian_Lewis} title= "Brian Lewis" text="Junior Developer"></Card> 
                     </div>
                     <div className="col-lx">
-                        <Card imgsrc={{}} title="Brian Lewis" text="Junior Developer" />
+                        <Card imgsrc= {Gio_Nava} title= "Giovanni Nava" text="Junior Developer"/>
                     </div>
                     <div className="col-lx">
-                        <Card imgsrc={{}} title="Chris Whiting" text="Junior Developer" />
+                        <Card imgsrc={Chris_Whiting} title= "Chris Whiting" text="Junior Developer"/>
                     </div>
                     <div className="col-lx">
-                        <Card imgsrc={Beckie_Rich} title="Beckie Rich" text="Junior Developer" />
+                        <Card imgsrc={Beckie_Rich} title= "Beckie Rich" text="Junior Developer"/>
                     </div>
                     <div className="col-lx">
-                        <Card imgsrc={Thompson_Scott} title="Scott Thompson" text="Junior Developer" />
+                        <Card imgsrc={Scott_Thompson} title= "Scott Thompson" text="Junior Developer"/>
                     </div>
                 </div>
             </div>
--- client-react/src/components/Data.js
+++ client-react/src/components/Data.js
@@ -33,7 +33,7 @@ export const AboutObjThree = {
     LightTextDesc: true,
     topLine: 'Step One of Code will be entered here',
     headLine: 'What is Lorem Ipsum?',
-    description: 'What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    description: 'React.js has multiple advantages for developing applications such as ours. React is focused and easy to learn, it comes with a helpful set of developer tools, and it guarantees stable code just to name a few. We really enjoyed digging into React and discovering what how it could help make our application the best it could be. ',
     buttonLabel: 'Copy Code',
     imgStart: 'start',
     img: 'React-icon.svg',
