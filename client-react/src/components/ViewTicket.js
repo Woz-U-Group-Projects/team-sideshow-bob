955b4bc3dee6537711d858fb8c2da8bded027304
Brian Lewis <briandeanlewis@gmail.com>
2020-11-04 20:46:22 -0500

    MInor Edit

--- client-react/src/components/ViewTicket.js
+++ client-react/src/components/ViewTicket.js
@@ -83,7 +83,7 @@ class ViewTicket extends React.Component {
       var data = [];
       var item;
       for (item of this.state.tickets) {
-        data.push({ "id": item.id, "First_Name": item.firstName, "Last_Name": item.lastName, "Email": item.email, "Urgency": item.urgency, "Complete": item.complete });
+        data.push({ "id": item.id, "First_Name": item.firstName, "Last_Name": item.lastName, "Email": item.email, "Urgency": item.urgency, "Category": item.category, "Complete": item.complete });
       }
       table= <ReactFlexyTable data={data} additionalCols={additionalCols} sortable globalSearch />
     }
